477a6bfedbc248ed639196a60f6de14689db8db8
MJ Rossetti <s2t2@users.noreply.github.com>
2021-02-03 18:19:25 -0500

    Validate user inputs

--- game.py
+++ game.py
@@ -16,6 +16,27 @@ user_choice = input("Please choose either 'rock', 'paper', or 'scissors':")
 print(f"You chose: {user_choice}")
 
 #
+# validate the user selection
+#
+# stop the program (not try to determine the winner)
+# ... if the user choice is invalid
+
+#user_choice.lower()
+
+#if user_choice in options:
+#    #print("GOOD")
+#    pass
+#else:
+#    print("OOPS, please choose a valid option and try again")
+#    exit()
+
+options = ["rock", "paper", "scissors"]
+
+if user_choice not in options:
+    print("OOPS, please choose a valid option and try again")
+    exit()
+
+#
 # simulating a computer input
 #
 
@@ -25,25 +46,24 @@ print(f"You chose: {user_choice}")
 #foo = ['a', 'b', 'c', 'd', 'e']
 #computer_choice = random.choice(foo)
 
-options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 #computer_choice = choice(options)
 
 print(f"The computer chose: {computer_choice}")
 
 
-#
-# determing who won
-#
 
+#
+# determine who won
+#
 
 if user_choice == computer_choice:
     print("It's tie!")
-elif (user_choice == "paper" and computer_choice == "rock"):
+elif user_choice == "paper" and computer_choice == "rock":
     print("You win! Congrats")
-elif (user_choice == "paper" and computer_choice == "scissors"):
+elif user_choice == "paper" and computer_choice == "scissors":
     print("Oh! The computer won, that's ok!")
-elif (user_choice == "rock" and computer_choice == "paper"):
+elif user_choice == "rock" and computer_choice == "paper":
     print("Oh! The computer won, that's ok!")
 elif user_choice == "rock" and computer_choice == "scissors":
     print("You win! Congrats")
@@ -51,6 +71,9 @@ elif user_choice == "scissors" and computer_choice == "paper":
     print("You win! Congrats")
 elif user_choice == "scissors" and computer_choice == "rock":
     print("Oh! The computer won, that's ok!")
+
+
+
 
 
 
